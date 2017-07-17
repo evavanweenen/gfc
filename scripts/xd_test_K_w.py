3a077ec0a989a1d7b64d638e487eeffbc4e631c8
Olivier Burggraaff <olivierburggraaff@gmail.com>
2017-07-17 11:59:18 +0200

    Update xd_test_K_w.py

--- scripts/xd_test_K_w.py
+++ scripts/xd_test_K_w.py
@@ -1,22 +1,22 @@
-import gaia_fc as g
+import gfc
 
-sqwrange = g.np.arange(0.5, 3.75, 0.25)
+sqwrange = gfc.np.arange(0.5, 3.75, 0.25)
 wrange = sqwrange**2.
 Krange = range(4, 21)[13:]
 for w in wrange:
     f_w = "tgas/2MASS/XD_K_w/w_{0}".format(w)
-    if not g.isdir(f_w):
-        g.mkdir(f_w)
+    if not gfc.isdir(f_w):
+        gfc.mkdir(f_w)
     for K in Krange:
         f = "tgas/2MASS/XD_K_w/w_{0}/K_{1}".format(w, K)
-        if not g.isdir(f):
-            g.mkdir(f)
+        if not gfc.isdir(f):
+            gfc.mkdir(f)
 
-t = g.io.load_table_with_separate_arrays(saveto_folder="tgas/2MASS/results/")
+t = gfc.io.load_table_with_separate_arrays(saveto_folder="tgas/2MASS/results/")
 
-warr = g.XD_arr(t, "w1", "w2", "w3")
-wcov = g.XD_arr(t, "S") ; wcov[:, 0, 0] = 1e15
-proj = g.XD_arr(t, "R")
+warr = gfc.XD_arr(t, "w1", "w2", "w3")
+wcov = gfc.XD_arr(t, "S") ; wcov[:, 0, 0] = 1e15
+proj = gfc.XD_arr(t, "R")
 
 initial_amps = [23.92, 22.78, 22.62, 8.91, 8.51, 6.74, 3.98, 1.74, 0.72, 0.09, 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.] # %
 
@@ -61,7 +61,7 @@ initial_covs = [(700., -111., -60., 200., 25., 145.),
                 (12., 0., 0., 17., 0., 10.),
                 (11., 0., 0., 18., 0., 10.),
                 (10., 0., 0., 19., 0., 10.),]
-initial_covs = [g.covariant_array(cov) for cov in initial_covs]
+initial_covs = [gfc.covariant_array(cov) for cov in initial_covs]
 
 for K in Krange:
     for w in wrange:
@@ -69,23 +69,5 @@ for K in Krange:
         print "K = {0} ; w = {1}".format(K, w),  ; g.io.flush()
         amps_xd, means_xd, covs_xd, L = g.XD(warr, wcov, initial_amps[:K], initial_means[:K], initial_covs[:K], projection = proj, w = w)
         print >> open(f+"/L", 'w'), L
-        g.io.save_PDFs(amps_xd, means_xd, covs_xd, f)
+        gfc.io.save_PDFs(amps_xd, means_xd, covs_xd, f)
         print "done!"
-        #PDFs = map(g.pdf.multivariate, means_xd, covs_xd, amps_xd)
-        #g.gplot.PDFs(PDFs, "xy", saveto=f+"/PDFs-U_V.png".format(K), xlim=(-130, 120), ylim=(-120, 60), title="TGAS-2MASS XD with $K = {0}$ Gaussians: $U$ vs $V$".format(K), xlabel="$U$ [km/s]", ylabel="$V$ [km/s]")
-        #g.gplot.PDFs(PDFs, "xz", saveto=f+"/PDFs-U_W.png".format(K), xlim=(-130, 120), ylim=(-70, 70),  title="TGAS-2MASS XD with $K = {0}$ Gaussians: $U$ vs $W$".format(K), xlabel="$U$ [km/s]", ylabel="$W$ [km/s]")
-        #g.gplot.PDFs(PDFs, "yz", saveto=f+"/PDFs-V_W.png".format(K), xlim=(-130, 120), ylim=(-70, 70),  title="TGAS-2MASS XD with $K = {0}$ Gaussians: $V$ vs $W$".format(K), xlabel="$V$ [km/s]", ylabel="$W$ [km/s]")
-        #print "Plotted individual PDFs"
-        #evaluated = g.pdf.eval_total_PDF(PDFs, [(-201,201), (-199,199), (-200,200)])
-        #evalxy, evalxz, evalyz = g.pdf.flatten_to_2D(evaluated)
-        #g.gplot.PDFs_gradients(evalxy, vmin=1e-5, extent=(-201,201,-199,199), xlim=(-110,110), ylim=(-120,60), xlabel="$U$ (km/s)", ylabel="$V$ (km/s)", title="TGAS-2MASS XD with $K = {0}$ Gaussians: $U$ vs $V$".format(K), saveto=f+"/PDFs_gradient-U_V.png".format(K))
-        #g.gplot.PDFs_gradients(evalxz, vmin=1e-5, extent=(-201,201,-200,200), xlim=(-110,110), ylim=(-70,70),  xlabel="$U$ (km/s)", ylabel="$W$ (km/s)", title="TGAS-2MASS XD with $K = {0}$ Gaussians: $U$ vs $V$".format(K), saveto=f+"/PDFs_gradient-U_W.png".format(K))
-        #g.gplot.PDFs_gradients(evalyz, vmin=1e-5, extent=(-199,199,-200,200), xlim=(-110,110), ylim=(-70,70),  xlabel="$V$ (km/s)", ylabel="$W$ (km/s)", title="TGAS-2MASS XD with $K = {0}$ Gaussians: $U$ vs $V$".format(K), saveto=f+"/PDFs_gradient-V_W.png".format(K))
-        #print "Plotted PDF gradients"
-
-#g.gplot.plt.plot(*zip(*Ls), c='k', lw=2)
-#g.gplot.plt.xlabel("$K$")
-#g.gplot.plt.ylabel("Likelihood")
-#g.gplot.plt.tight_layout()
-#g.gplot.plt.savefig("tgas/2MASS/K_L.png")
-#g.gplot.plt.close()
