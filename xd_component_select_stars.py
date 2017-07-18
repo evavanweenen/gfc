07405cedd8b36e6eb3a72ef3eb293af11d86bd83
Olivier Burggraaff <burggraaff@pczaal0.strw.leidenuniv.nl>
2017-07-18 17:46:59 +0200

    Component selection

--- xd_component_select_stars.py
+++ xd_component_select_stars.py
@@ -8,6 +8,7 @@ parser.add_argument("xd_results_folder", help = "Folder that contains the XD res
 parser.add_argument("save_folder", help = "Folder in which to save results")
 parser.add_argument("-v", "--verbose", action = "store_true")
 parser.add_argument("-n", help = "Which component to plot", type = int, default = 0)
+parser.add_argument("-t", "--threshold", help = "Log-likelihood threshold for component membership", default = -9)
 args = parser.parse_args()
 
 t = gfc.io.read_csv(args.data_file)
@@ -37,11 +38,17 @@ Ls = gfc.pdf.likelihood_many(PDFs, t["UVW_vec"])
 if args.verbose:
     print "Calculated likelihoods"
 
-t.add_column(gfc.table.Column(data = Ls[:, args.n], name = "L"))
-t.sort("L")
-t.reverse()
-gfc.gplot.plt.scatter(t["U"][:5000], t["W"][:5000])
-gfc.gplot.plt.xlim(-120,120)
-gfc.gplot.plt.ylim(-70,70)
-gfc.gplot.draw_PDF_ellipse(gfc.gplot.plt.gca(), amps_xd[args.n], means_xd[args.n], covs_xd[args.n], "xz")
-gfc.gplot.plt.savefig(args.save_folder + "stars.png")
+for n in range(19):
+    t.add_column(gfc.table.Column(data = Ls[:, n], name = "L"))
+    t.sort("L")
+    t.reverse()
+    gfc.gplot.plt.hist2d(t["U"][:5000], t["W"][:5000], range = ((-120, 120), (-70, 70)), bins = 100)
+    gfc.gplot.plt.xlim(-120,120)
+    gfc.gplot.plt.ylim(-70,70)
+    gfc.gplot.draw_PDF_ellipse(gfc.gplot.plt.gca(), amps_xd[n], means_xd[n], covs_xd[n], "xz")
+    gfc.gplot.plt.savefig(args.save_folder + "stars_{n}.png".format(n = n))
+    gfc.gplot.plt.close()
+    t.remove_column("L")
+#for N in range(19):
+#    Ls[:,N] = np.log(Ls[:,N])
+#    print "{N}: min {min:.1f} ; median {median:.1f} ; 95% {ten:.1f} ; max {max:.1f} ".format(N = N, max = np.nanmax(Ls[:,N]), min = np.nanmin(Ls[:,N]), median = np.median(Ls[:,N]), ten = np.percentile(Ls[:,N], 95))
