d80de53e320ee5850af67e6e3eea6bd573e36dca
Olivier Burggraaff <olivierburggraaff@gmail.com>
2017-07-19 14:43:19 +0200

    Stars outside moving groups

--- xd_component_select_stars.py
+++ xd_component_select_stars.py
@@ -36,10 +36,11 @@ Ls = gfc.pdf.loglikelihood_many_multiPDF(means_xd, covs_xd, t["UVW_vec"])
 if args.verbose:
     print "Calculated likelihoods"
 
-n = 0
-gfc.gplot.moving_group(t["ra"], t["dec"], t["l"], t["b"], t["U"], t["V"], t["W"], bins = 100, saveto = "all_mg.png") 
-t_subs = [t[Ls[:,n] > -args.threshold] for n in range(len(means_xd))]
-for n, t_ in enumerate(t_subs):
+gfc.gplot.moving_group(t["ra"], t["dec"], t["l"], t["b"], t["U"], t["V"], t["W"], range(len(t)), [], bins = 100, saveto = "all_mg.png") 
+for n, (a, m, c) in enumerate(zip(amps_xd, means_xd, covs_xd)):
+    indices_in = np.where(Ls[:,n] > -args.threshold)[0]
+    indices_ex = np.where(Ls[:,n] <= -args.threshold)[0]
     if args.verbose:
         print "{n:02d}: {l}".format(n=n, l=len(t_))
-    gfc.gplot.moving_group(t_["ra"], t_["dec"], t_["l"], t_["b"], t_["U"], t_["V"], t_["W"], a = amps_xd[n], m = means_xd[n], c = covs_xd[n], bins = 100, saveto = "{0:02d}_mg.png".format(n+1)) 
+    gfc.gplot.moving_group(t_["ra"], t_["dec"], t_["l"], t_["b"], t_["U"], t_["V"], t_["W"], indices_in, indides_ex, \
+        a = a, m = m, c = c, bins = 100, saveto = "{f}/{n:02d}_mg.png".format(f = args.save_folder, n = n+1)) 
