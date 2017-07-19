a0316f1e701b68442ec13be2b0238bd3af93f8cc
Olivier Burggraaff <burggraaff@pczaal0.strw.leidenuniv.nl>
2017-07-19 08:46:59 +0200

    Documentation in __init__ files

--- gfc/__init__.py
+++ gfc/__init__.py
@@ -1,5 +1,20 @@
 """
-__init__ file for the gfc module
+gfc: a module for processing and interpreting data from ESA's Gaia satellite, with a particular focus on the extreme deconvolution (XD) algorithm.
+
+Submodules:
+
+gfc.tgas
+    Functions specific to the TGAS dataset
+gfc.pdf
+    Functions for dealing with probability density functions (PDFs)
+gfc.io
+    Functions for input and output, to file and to screen
+gfc.mapping
+    Functions for mapping other functions over large datasets, using multiprocessing and numpy magic where possible
+gfc.gplot
+    Functions for easily making plots that one is likely to make many times
+gfc.general:
+    General functions and constants; these are all imported directly into gfc
 """
 from . import tgas
 from . import pdf
