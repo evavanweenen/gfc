da4ee97f57a58d25e5c0d946c55c3cb14e33f888
Olivier Burggraaff <burggraaff@dollard.strw.leidenuniv.nl>
2017-07-20 12:06:05 +0200

    Use angle

--- select_ring.py
+++ select_ring.py
@@ -6,7 +6,7 @@ parser = ArgumentParser()
 parser.add_argument("data_file", help = "File that contains the stellar data")
 parser.add_argument("xd_results_folder", help = "Folder that contains the XD results")
 parser.add_argument("save_folder", help = "Folder in which to save results")
-parser.add_argument("-t", "--threshold", type = float, help = "Threshold on dot product to select stars", default = 0.05)
+parser.add_argument("-t", "--threshold", type = float, help = "Threshold angle to select stars", default = 1.)
 parser.add_argument("-v", "--verbose", action = "store_true")
 args = parser.parse_args()
 
@@ -29,11 +29,12 @@ time_after_dot = gfc.time()
 if args.verbose:
     print "Calculated dot products in {0:.1f} seconds".format(time_after_dot - time_before_dot)
 
-selections = [np.where(d < args.threshold) for d in dot_products]
+crit = 1. - np.cos(args.threshold * np.pi / 180.)
+
+selections = [np.where(np.abs(d) < crit)[0] for d in dot_products]
 
 for i, (m, s) in enumerate(zip(mean_coords, selections)):
-    gfc.gplot.plt.scatter(m[0], m[1], s = 20, c='k')
-    gfc.gplot.plt.scatter(t["ra_rad"][s], t["dec_rad"][s], s = 1, color='r')
-    gfc.gplot.plt.savefig("{f}/ring_{n}.png".format(f = args.save_folder, n = i))
-    gfc.gplot.plt.close()
-    print i
+    #gfc.gplot.plt.scatter(m[0], m[1], s = 20, c='k')
+    #gfc.gplot.plt.scatter(t["ra_rad"][s], t["dec_rad"][s], s = 1, color='r')
+    gfc.gplot.density(t["ra"][s], t["dec"][s], r = ((0, 360), (-90, 90)), bins = 100, saveto = "{f}/ring_{n}.png".format(f = args.save_folder, n = i))
+    print "{n:02d}: {N:04d} stars".format(n = i + 1, N = len(s))
